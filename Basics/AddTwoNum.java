ffec8f6e039f5d7ffeb94c27fd16848c33b55324
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-23 21:26:57 +0530

    Create AddTwoNum.java

---
+++ Basics/AddTwoNum.java
@@ -0,0 +1,15 @@
+public class AddTwoNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int a = 12;
+        int b = 13;
+        
+        a+=2;
+        
+        int sum = a+b;
+        
+        System.out.println(sum);
+        }
+
+}
