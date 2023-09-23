9328bd8dd21288aab6342f928a8b26f6c38d9c30
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-23 21:32:00 +0530

    Update print.java

--- Basics/print.java
+++ Basics/print.java
@@ -2,7 +2,7 @@ public class print {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.print("Welcome to Coding Blocks");
+        System.out.print("Welcome to GitHub");
         System.out.println(" Hello");
         System.out.print(" Bye-bye");
         
