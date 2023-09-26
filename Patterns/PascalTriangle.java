3bc340fe0f5b4315e10010e572c3963baf9bce5a
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-26 08:46:34 +0530

    Add files via upload

---
+++ Patterns/PascalTriangle.java
@@ -0,0 +1,30 @@
+package lect5;
+import java.util.*;
+
+public class PascalTriangle  {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner s = new Scanner(System.in);
+        
+        int n = s.nextInt();
+        int row = 0;
+        int rowval = 1;
+        
+        while(row<n) {
+        	//Print star
+        	int i = 0;
+        	int ncr = rowval;
+        	while(i<=row) {
+        		System.out.print(ncr + " ");
+        		ncr = (ncr * (row-i))/(i+1);
+        		i++;
+        	}
+        	
+        	//Prep for next row
+        	row++;
+        	System.out.println();
+        }
+	}
+
+}
