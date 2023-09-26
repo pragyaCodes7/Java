2b7988ec1857e7284859e91c4c3492eb901b7c26
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-27 01:33:56 +0530

    Add files via upload

---
+++ Basics/DataTypes.java
@@ -0,0 +1,19 @@
+package lect6;
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Integer.toBinaryString(-8));
+		System.out.println(Long.toBinaryString(-8));
+		System.out.println(Integer.toBinaryString(-10));
+		
+		int x = Integer.MIN_VALUE;
+		int y = Integer.MAX_VALUE;
+		System.out.println(x);
+		System.out.println(y+1);
+        
+	}
+
+}
