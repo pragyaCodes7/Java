007bced174669e4b12c095cf805df10233949e1a
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-26 08:47:50 +0530

    Add files via upload

---
+++ Basics/SumOfOdd_EvenPlace.java
@@ -0,0 +1,30 @@
+package lect5;
+import java.util.*;
+public class SumOfOdd_EvenPlace {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner s = new Scanner(System.in);
+        
+        int n = s.nextInt();
+        int sumOdd = 0;
+        int sumEven = 0;
+        int count = 0;
+        
+        while(n>0) {
+        	int dig = n%10;
+        	count++;
+        	if(count%2 == 0) {
+        		sumEven += dig;
+        	} else {
+        		sumOdd += dig;
+        	}
+        	n /= 10;
+        }
+        
+        System.out.println(sumEven);
+        System.out.println(sumOdd);
+        
+	}
+
+}
