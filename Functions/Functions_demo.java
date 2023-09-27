8239447df162a16fd5f2364172b4566962adff81
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-28 02:38:29 +0530

    Create Functions_demo.java

---
+++ Functions/Functions_demo.java
@@ -0,0 +1,23 @@
+public class Functions_demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Main starts");
+		
+		addTwoNum();
+		
+		System.out.println("Main ends here");
+	}
+	
+	// Take nothing, return nothing
+	
+	 public static void addTwoNum() {
+		 
+		 System.out.println("addTwoNum function starts");
+		 
+		 System.out.println("addTwoNum function ends");
+		 
+	 }
+
+}
