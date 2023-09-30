1bcb84d066d7711f9033b3c00cddf8e520607eff
Pragya Rastogi <139483255+pragyaCodes7@users.noreply.github.com>
2023-09-30 14:24:33 +0530

    Create ArrayQues.java

---
+++ Arrays/ArrayQues.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class ArrayQues {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     
+	   Scanner s= new Scanner(System.in);
+	   int n= s.nextInt();
+	   
+	   int [] arr= new int[n];
+	   
+	   for(int i=0; i<n;i++) {
+		   arr[i]= (i+1)*100;
+	   }
+	   
+	   for(int i=0; i<n;i++) {
+		   System.out.print(arr[i]+" ");
+	   }
+    
+	}
+
+}
